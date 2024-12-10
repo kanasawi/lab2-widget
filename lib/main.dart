5cd09bcc8e3532e1fedecb01bcd16ec6ba7affa6
akimidus <kanassanan.wit@gmail.com>
2024-12-10 19:32:42 +0700

    create app

--- lib/main.dart
+++ lib/main.dart
@@ -6,120 +6,103 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(color: Colors.purple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+          useMaterial3: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("PEA VOLTA", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+          ),
+          body: Column(
+            children: [
+              const SizedBox(height: 8),
+              Image.asset("assets/images/pea.png", width: 100, height: 50,),
+              const SizedBox(height: 8),
+              const Text("ขอบคุณที่ใช้บริการ",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const Text("เรายินดีที่ได้เป็นส่วนหนึ่งในการเดินทางของคุณ",
+                  style:
+                      TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
+              const SizedBox(height: 32),
+              const Row(children: [
+                Text(
+                  "สรุปรายละเอียดการชาร์จ",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.date_range),
+                  ),
+                  const Text("วันที่ชาร์จ", style: TextStyle(fontSize: 16),),
+                  const Spacer(),
+                  const Text("9 ธันวาคม 2567", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.battery_0_bar),
+                  ),
+                  const Text("สถานีชาร์จ", style: TextStyle(fontSize: 16),),
+                  const Spacer(),
+                  const Text("PEA VOLTA บางจาก", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.electric_car),
+                  ),
+                  const Text("ประเภทหัวชาร์จ", style: TextStyle(fontSize: 16),),
+                  const Spacer(),
+                  const Text("CCS2", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.timelapse),
+                  ),
+                  const Text("ระยะเวลาการชาร์จ", style: TextStyle(fontSize: 16),),
+                  const Spacer(),
+                  const Text("00:12:32", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.shopping_bag),
+                  ),
+                  const Text("จำนวนหน่วย", style: TextStyle(fontSize: 16),),
+                  const Spacer(),
+                  const Text("9,314 kWh", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Row(
+                children: [
+                  Text("ค่าบริการทั้งสิ้น", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  Spacer(),
+                  Text("55.34 บาท", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
